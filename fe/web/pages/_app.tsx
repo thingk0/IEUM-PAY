999851c2f0d9a115d0d0a4ed2756230be1aacf60
김예지 <fpssj@naver.com>
2024-03-07 15:44:12 +0900

    ➕ depend: eslint+prettier 설정 추가

--- fe/web/pages/_app.tsx
+++ fe/web/pages/_app.tsx
@@ -1,16 +1,17 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import React from 'react';
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  if (process.env.NODE_ENV === "development") {
-    if (typeof window === "undefined") {
+  if (process.env.NODE_ENV === 'development') {
+    if (typeof window === 'undefined') {
       (async () => {
-        const { server } = await import("../mocks/server");
+        const { server } = await import('../mocks/server');
         server.listen();
       })();
     } else {
       (async () => {
-        const { worker } = await import("../mocks/browser");
+        const { worker } = await import('../mocks/browser');
         worker.start();
       })();
     }
