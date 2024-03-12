c2fd7a391a1f99800404adb1ec8efac27d6cad6f
김범수 <kkkbs41@daum.net>
2024-03-12 11:04:49 +0900

    🎉 begin: be 프로젝트 생성

---
+++ be/funding/src/test/java/com/ieum/funding/FundingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ieum.funding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
