16230e0c57685ac656368af65b0ed45829fc7e95
김예지 <fpssj@naver.com>
2024-03-18 13:01:47 +0900

    ✨ feat: 하단바 mvp 스토리북에 추가

---
+++ fe/web/stories/icons/CreditCardIconActive.tsx
@@ -0,0 +1,23 @@
+export default function CreditCardIcon() {
+  return (
+    <svg
+      width="32"
+      height="32"
+      viewBox="0 0 32 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_147_822)">
+        <path
+          d="M26.6667 5.33333H5.33332C3.85332 5.33333 2.67999 6.51999 2.67999 7.99999L2.66666 24C2.66666 25.48 3.85332 26.6667 5.33332 26.6667H26.6667C28.1467 26.6667 29.3333 25.48 29.3333 24V7.99999C29.3333 6.51999 28.1467 5.33333 26.6667 5.33333ZM26.6667 24H5.33332V16H26.6667V24ZM26.6667 10.6667H5.33332V7.99999H26.6667V10.6667Z"
+          fill="#8035F9"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_147_822">
+          <rect width="32" height="32" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
