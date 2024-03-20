77614d430ca51d6378ecb6fa609a4e87f029d51c
김예지 <fpssj@naver.com>
2024-03-20 09:56:29 +0900

    ✨ feat: pwa 기능 추가
    Change-Id: I36a8d1b653b10aaa691d5da4d266daea7f1c35b4

--- fe/web/components/Button.tsx
+++ fe/web/components/Button.tsx
@@ -3,7 +3,7 @@ import styles from './button.module.scss';
 interface ButtonProps {
   text: string;
   btnStyle: string;
-  btnFunction: Function;
+  btnFunction: (e?: React.MouseEvent<HTMLButtonElement, MouseEvent>) => unknown;
 }
 export default function Button({ text, btnStyle, btnFunction }: ButtonProps) {
   return (
