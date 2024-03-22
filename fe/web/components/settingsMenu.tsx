2452677b1f2fc4678bad8ff0be46d3f98cc8615e
이은규 <eung9702@gmail.com>
2024-03-22 10:57:33 +0900

    ✨ feat: 설정 페이지 틀 완성 / HalfButton 컴포넌트 제작
    Change-Id: I6622fe4de7c6c3369614522e7f2c0147eaf91fe2

--- fe/web/components/settingsMenu.tsx
+++ fe/web/components/settingsMenu.tsx
@@ -1,10 +1,10 @@
 import ChevronRightIcon from './icons/ChevronRightIcon';
 import styles from './settingsMenu.module.scss';
 
-interface SettingsMenu {
+interface SettingsMenuProps {
   title: string;
 }
-export default function SettingsMenu({ title }) {
+export default function SettingsMenu({ title }: SettingsMenuProps) {
   return (
     <>
       <div className={styles.container}>
--- fe/web/pages/settings/index.tsx
+++ fe/web/pages/settings/index.tsx
@@ -1,15 +1,20 @@
 import PageTitleLeft from '@/components/PageTitleLeft';
 import styles from './settings.module.scss';
 import SettingsMenu from '@/components/settingsMenu';
+import ChevronRightIcon from '@/components/icons/ChevronRightIcon';
+import HalfButton from '@/components/HalfButton';
 
 export default function Settings() {
   return (
     <div className={styles.container}>
-      <PageTitleLeft
-        title={'설정'}
-        description={'설정은 설명이 없지만 테스트용으로 써봤습니다잉.'}
-      />
-      <div className={styles.myBox}></div>
+      <PageTitleLeft title={'설정'} description={''} />
+      <div className={styles.myBox}>
+        <div className={styles.myBoxText}>
+          <p className={styles.name}>{'김범수'}</p>
+          <p className={styles.subText}>내 정보 수정하기</p>
+        </div>
+        <ChevronRightIcon />
+      </div>
       <div className={styles.menuBox}>
         <SettingsMenu title={'비밀번호 변경'} />
         <SettingsMenu title={'약관 및 정책'} />
