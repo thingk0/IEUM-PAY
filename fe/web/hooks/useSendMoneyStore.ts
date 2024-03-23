aff42376605ee59f148dce3a3231e1f8eab93245
김예지 <fpssj@naver.com>
2024-03-23 11:23:05 +0900

    🐛 fix: 금액 추가 버튼을 눌렀을때 키패드보다 한도 높은 버그 수정
    Change-Id: Idea3926e92e9737d223e78eb5596aa1698222c2a

--- fe/web/hooks/useSendMoneyStore.ts
+++ fe/web/hooks/useSendMoneyStore.ts
@@ -1,5 +1,7 @@
 import { create } from 'zustand';
 
+const MAX_TRANSFER = 100000000;
+
 interface sendMoneyInfoType {
   수취인: string;
   수취은행: string;
@@ -41,7 +43,7 @@ const useSendMoneyInfo = create<SendMoneyInfoState & SendMoneyInfoActions>(
           수취은행: state.sendMoneyInfo.수취은행,
           수취계좌: state.sendMoneyInfo.수취계좌,
           송금금액: Math.min(
-            100000000,
+            MAX_TRANSFER,
             state.sendMoneyInfo.송금금액 * 10 + amount,
           ),
           송금은행: state.sendMoneyInfo.송금은행,
@@ -67,7 +69,10 @@ const useSendMoneyInfo = create<SendMoneyInfoState & SendMoneyInfoActions>(
           수취인: state.sendMoneyInfo.수취인,
           수취은행: state.sendMoneyInfo.수취은행,
           수취계좌: state.sendMoneyInfo.수취계좌,
-          송금금액: state.sendMoneyInfo.송금금액 + amount,
+          송금금액: Math.min(
+            state.sendMoneyInfo.송금금액 + amount,
+            MAX_TRANSFER,
+          ),
           송금은행: state.sendMoneyInfo.송금은행,
           잔액: state.sendMoneyInfo.잔액,
         },
