bcfc10900bcef62b6f70dd9652ac73afd9af8fcb
김예지 <fpssj@naver.com>
2024-03-23 11:20:36 +0900

    ♻️ refactor: 진동 함수 유틸화
    Change-Id: I95ee4742397dd0bc9ce4fff19b911c3cf24e53e7

--- fe/web/components/AmountButton.tsx
+++ fe/web/components/AmountButton.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import classes from './AmountButton.module.css';
+import Vibrate from '@/utils/vibrate';
 interface AmountButtonProps {
   text: string;
   onClick: (e?: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 }
 function AmountButton({ text, onClick }: AmountButtonProps) {
+  function handleClick(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    Vibrate(10);
+    onClick(e);
+  }
   return (
-    <button
-      onClick={(e) => {
-        onClick(e);
-        navigator?.vibrate(10);
-      }}
-      className={classes.button}
-    >
+    <button onClick={handleClick} className={classes.button}>
       {text}
     </button>
   );
