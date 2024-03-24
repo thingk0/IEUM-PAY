fe2b454b66c21d7c8de4c1d37b53d083733b80a5
김예지 <fpssj@naver.com>
2024-03-24 10:32:37 +0900

    🐛 fix: title 태그 위치 수정
    경고 메시지 때문에
    
    Change-Id: Ia6f8b07593a8b624c0133c44885b8037791601f1

--- fe/web/pages/_app.tsx
+++ fe/web/pages/_app.tsx
@@ -22,6 +22,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <title>IEUM PAY</title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
