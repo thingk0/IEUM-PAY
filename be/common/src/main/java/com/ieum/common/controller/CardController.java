8eb03a5ffc77920f60e6be5a571828001d1a91e9
고명성 <audtjd9080@naver.com>
2024-03-25 01:55:08 +0900

    ✨ feat: ResponseTemplate 로 응답 방식 통합
    - 카드 관련 기능 (메인 카드 설정, OCR 처리, 카드 등록, 카드 정보 수정)을 처리하는 CardController 내의 응답 로직을 ResponseTemplate 을 사용하여 일관성 있게 리팩토링함.

--- be/common/src/main/java/com/ieum/common/controller/CardController.java
+++ be/common/src/main/java/com/ieum/common/controller/CardController.java
@@ -1,10 +1,10 @@
 package com.ieum.common.controller;
 
-import com.ieum.common.request.CardMainRequestDTO;
-import com.ieum.common.request.CardRegisterRequestDTO;
-import com.ieum.common.request.CardUpdateRequestDTO;
-import com.ieum.common.response.CardOcrResponseDTO;
-import com.ieum.common.response.CardRegisterResponseDTO;
+import com.ieum.common.dto.request.CardMainRequestDTO;
+import com.ieum.common.dto.request.CardRegisterRequestDTO;
+import com.ieum.common.dto.request.CardUpdateRequestDTO;
+import com.ieum.common.dto.response.CardOcrResponseDTO;
+import com.ieum.common.format.response.ResponseTemplate;
 import com.ieum.common.service.PayService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -15,50 +15,56 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+@Tag(name = "card", description = "card API - 목업")
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/card")
-@RequiredArgsConstructor
-@Tag(name = "card", description = "card API - 목업")
 public class CardController {
 
     private final PayService payService;
+    private final ResponseTemplate response;
+
 
     @Operation(summary = "메인 카드 설정", description = "사용자의 메인 카드를 설정합니다.")
     @ApiResponse(responseCode = "200", description = "메인 카드 설정 성공")
     @PutMapping("/main")
-    public ResponseEntity<HttpStatus> cardMain(@RequestBody CardMainRequestDTO cardMainRequestDTO){
-        return new ResponseEntity<>(HttpStatus.OK);
+    public ResponseEntity<?> cardMain(@RequestBody CardMainRequestDTO cardMainRequestDTO) {
+        return response.success(HttpStatus.OK);
     }
+
     @Operation(summary = "카드 OCR 처리", description = "카드 이미지를 스캔하여 정보를 추출합니다.")
     @ApiResponse(responseCode = "200", description = "OCR 처리 성공 - OCR 통해 읽은 카드 정보 반환")
     @PostMapping("/ocr")
-    public ResponseEntity<CardOcrResponseDTO> cardOcr(@RequestParam("img") MultipartFile img) {
+    public ResponseEntity<?> cardOcr(@RequestParam("img") MultipartFile img) {
         CardOcrResponseDTO cardOcrResponseDTO = payService.getOcr(img);
-        return new ResponseEntity<>(cardOcrResponseDTO,HttpStatus.OK);
+        return response.success(HttpStatus.OK);
     }
 
     @Operation(summary = "카드 등록", description = "새로운 카드를 등록합니다.")
     @ApiResponse(responseCode = "200", description = "카드 등록 성공")
     @ApiResponse(responseCode = "500", description = "카드 등록 실패")
     @PostMapping("/register")
-    public ResponseEntity<CardRegisterResponseDTO> cardRegister(@RequestBody CardRegisterRequestDTO cardRegisterRequesterDTO){
+    public ResponseEntity<?> cardRegister(@RequestBody CardRegisterRequestDTO cardRegisterRequesterDTO) {
         Long id = payService.cardRegister(cardRegisterRequesterDTO);
-        CardRegisterResponseDTO responseDTO = CardRegisterResponseDTO.builder().cardId(id).build();
+//        CardRegisterResponseDTO responseDTO = CardRegisterResponseDTO.builder().cardId(id).build();
 
-        if(id == -1L)
-            return new ResponseEntity<>(responseDTO,HttpStatus.BAD_REQUEST);
-        else if(id == -2L)
-            return new ResponseEntity<>(responseDTO,HttpStatus.BAD_REQUEST);
-        return new ResponseEntity<>(responseDTO, HttpStatus.OK);
+        /*if (id == -1L) {
+            return new ResponseEntity<>(responseDTO, HttpStatus.BAD_REQUEST);
+        } else if (id == -2L) {
+            return new ResponseEntity<>(responseDTO, HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(responseDTO, HttpStatus.OK);*/
+
+        return response.success(HttpStatus.OK);
     }
 
     @Operation(summary = "카드 삭체", description = "카드 정보를 삭제합니다.")
     @ApiResponse(responseCode = "200", description = "카드 삭제 성공")
     @PutMapping("/update")
-    public ResponseEntity<HttpStatus> cardUpdate(@RequestBody CardUpdateRequestDTO cardUpdateRequestDTO){
-        //auth check
-        //card owner is user?
+    public ResponseEntity<?> cardUpdate(@RequestBody CardUpdateRequestDTO cardUpdateRequestDTO) {
+//        auth check
+//        card owner is user?
         payService.updateCard(cardUpdateRequestDTO.getRegisteredCardId());
-        return new ResponseEntity<>(HttpStatus.OK);
+        return response.success(HttpStatus.OK);
     }
 }
