81f3fdd773b3e5e285913c452ae309d3d9e9bc10
고명성 <audtjd9080@naver.com>
2024-03-24 23:16:48 +0900

    ✨ feat: MMSController를 AuthController로 변경 및 로직 개선
    - MmsController의 기능을 AuthController로 옮기고, MMS 인증 로직을 개선하여 보다 명확하고 안정적인 인증 처리를 할 수 있도록 변경
    - 인증 코드 발급 방식을 UUID를 사용한 무작위 코드 생성으로 변경하여 보안 강화
    - StringRedisTemplate 를 사용한 코드 저장 및 만료 시간 설정을 통해 임시 인증 코드 관리 개선
    - 입력된 휴대폰 번호의 유효성 검사 로직 추가하여 요청 데이터의 정확성 보장

---
+++ be/common/src/main/java/com/ieum/common/controller/AuthController.java
@@ -0,0 +1,43 @@
+package com.ieum.common.controller;
+
+import com.ieum.common.format.response.ResponseTemplate;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "auth", description = "인증 API - 목업")
+@Validated
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final ResponseTemplate response;
+    private final StringRedisTemplate stringRedisTemplate;
+
+    @Operation(summary = "본인 확인용 인증 메시지 발급", description = "본인 확인 메시지 요청을 받아 인증 코드를 반환합니다.")
+    @ApiResponse(responseCode = "200", description = "본인 확인 메시지용 코드 반환")
+    @GetMapping("/api/auth")
+    public ResponseEntity<?> getAuthenticationCode(@RequestParam("phone-number")
+                                                   @NotBlank(message = "휴대폰 번호는 필수입니다.")
+                                                   @Pattern(regexp = "^010-\\d{4}-\\d{4}$", message = "휴대폰 번호 형식이 잘못되었습니다.")
+                                                   String phoneNumber) {
+
+        String code = "code:" + UUID.randomUUID() + "%";
+        stringRedisTemplate.opsForValue().set("phone-number:" + phoneNumber,
+                                              code, 5, TimeUnit.MINUTES);
+        return response.success(code, HttpStatus.OK);
+    }
+
+}
