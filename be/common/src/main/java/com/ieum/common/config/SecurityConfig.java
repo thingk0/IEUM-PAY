7868ffad3a36f6fc99f1738e8396c4ebb48c30a6
고명성 <audtjd9080@naver.com>
2024-03-24 23:24:19 +0900

    🔒 security: Security 설정 업데이트 및 JWT 인증 필터 추가
    - 기존 보안 설정에서 JWT 를 활용한 상태로 업데이트하여 애플리케이션 보안 강화
    - BCryptPasswordEncoder 를 사용한 비밀번호 암호화 방식 적용
    - SessionCreationPolicy 를 STATELESS 로 설정하여 세션 기반 인증 방식 해지
    - JWTAuthenticationFilter 추가로 인증 토큰 검증 과정 자동화 및 보안성 증가
    - 특정 경로에 대한 접근 허용 및 나머지 요청에 대한 인증 요구로 보안 정책 세분화

--- be/common/src/main/java/com/ieum/common/config/SecurityConfig.java
+++ be/common/src/main/java/com/ieum/common/config/SecurityConfig.java
@@ -1,43 +1,68 @@
 package com.ieum.common.config;
 
-import lombok.AllArgsConstructor;
+import com.ieum.common.filter.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@EnableWebSecurity
 @Configuration
-@AllArgsConstructor
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity
-            .httpBasic().disable()
-            .formLogin().disable() // 폼 로그인 비활성화
-            .csrf().disable()
-            .cors().and()
-            .headers().frameOptions().disable().and()
-            .authorizeRequests()
-            .antMatchers("/**","/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html", "/swagger-resources/**", "/webjars/**").permitAll() // Swagger에 대한 접근 허용
-            .anyRequest().authenticated().and()
-            .build();
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOriginPattern("*");
-        configuration.addAllowedHeader("*");
-        configuration.addAllowedMethod("*");
-        configuration.addExposedHeader("*");
-        configuration.setAllowCredentials(true);
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
+    public SecurityFilterChain filterChain(HttpSecurity security) throws Exception {
+
+        security
+            .httpBasic(basic -> basic.disable())
+            .csrf(csrf -> csrf.disable())
+            .cors(cors -> cors.disable())
+        ;
+
+        security
+            .authorizeHttpRequests((authorize ->
+            {
+                authorize.antMatchers(
+                    "/api-docs/**",
+                    "/v2/api-docs/**",
+                    "/v3/api-docs/**",
+                    "/webjars/**",
+                    "/swagger/**",
+                    "/swagger-ui/**",
+                    "/swagger-config/**",
+                    "/swagger-resources/**",
+                    "/api/member/login",
+                    "/api/member/regist"
+                ).permitAll();
+                authorize.anyRequest().authenticated();
+            }))
+        ;
+
+        security
+            .sessionManagement(sessionManager -> {
+                sessionManager.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+            })
+        ;
+
+        security
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+        ;
+
+        return security.build();
     }
+
 }
