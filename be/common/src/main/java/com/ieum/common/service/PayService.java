8ed55ea7ef3fe846a2dd8ad6506b27ae7e27516c
고명성 <audtjd9080@naver.com>
2024-03-24 23:42:34 +0900

    ♻️ refactor: PayService 업데이트 및 PayFeignClient 인터페이스 변경 사항 적용
    - PayFeignClient 인터페이스의 메서드 명명 규칙 개선 및 주석 추가로 문서화 지원 강화
    - 카드 OCR, 카드 번호 유효성 검사, 카드 삭제 등의 메서드를 업데이트하여 외부 결제 시스템과의 연동 강화
    - 변경된 메서드를 사용하여 카드 관련 기능의 구현체 업데이트
    - 외부 결제 시스템과의 통신을 위한 서비스 레이어의 유연성 및 확장성 개선

--- be/common/src/main/java/com/ieum/common/service/PayService.java
+++ be/common/src/main/java/com/ieum/common/service/PayService.java
@@ -1,29 +1,29 @@
 package com.ieum.common.service;
 
+import com.ieum.common.dto.request.CardRegisterRequestDTO;
+import com.ieum.common.dto.response.CardOcrResponseDTO;
 import com.ieum.common.feign.PayFeignClient;
-import com.ieum.common.request.CardRegisterRequestDTO;
-import com.ieum.common.response.CardOcrResponseDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
-
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class PayService {
+
     private final PayFeignClient payFeignClient;
 
     public CardOcrResponseDTO getOcr(MultipartFile img) {
-        return payFeignClient.cardOcr(img);
+        return payFeignClient.getCardOcrResult(img);
     }
 
     public Long cardRegister(CardRegisterRequestDTO requesterDTO) {
-        return payFeignClient.cardValid(requesterDTO);
+        return payFeignClient.isCardNumberValid(requesterDTO);
     }
 
     public void updateCard(Long id) {
-        payFeignClient.cardDelete(id);
+        payFeignClient.deleteCard(id);
     }
 }
