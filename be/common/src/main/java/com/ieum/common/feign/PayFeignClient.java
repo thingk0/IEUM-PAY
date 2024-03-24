1fdca2288eaa9fddc684556e365b5250f2abeb9e
고명성 <audtjd9080@naver.com>
2024-03-24 23:30:55 +0900

    ✨ feat: PayFeignClient 인터페이스 업데이트 및 페이머니 생성 기능 추가
    - 카드 OCR, 카드 유효성 검사, 카드 삭제 기능에 대한 Feign 클라이언트 메서드 정의 업데이트 및 주석 추가로 문서화 지원 강화
    - 페이머니 생성 기능 추가로 사용자 결제 수단 관리 기능 확장

--- be/common/src/main/java/com/ieum/common/feign/PayFeignClient.java
+++ be/common/src/main/java/com/ieum/common/feign/PayFeignClient.java
@@ -1,7 +1,7 @@
 package com.ieum.common.feign;
 
-import com.ieum.common.request.CardRegisterRequestDTO;
-import com.ieum.common.response.CardOcrResponseDTO;
+import com.ieum.common.dto.request.CardRegisterRequestDTO;
+import com.ieum.common.dto.response.CardOcrResponseDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,12 +11,40 @@ import org.springframework.web.multipart.MultipartFile;
 
 @FeignClient(name = "pay", url = "${gateway.pay}")
 public interface PayFeignClient {
-    @PostMapping(value = "/card/ocr",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    CardOcrResponseDTO cardOcr(@RequestBody MultipartFile img);
 
-    @PostMapping("/card/valid")
-    Long cardValid(@RequestBody CardRegisterRequestDTO cardNumber);
+    /**
+     * 카드 OCR 기능을 제공하는 메서드
+     *
+     * @param img 카드 이미지 파일
+     * @return 카드 OCR 결과
+     */
+    @PostMapping(value = "/card/ocr", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    CardOcrResponseDTO getCardOcrResult(@RequestBody MultipartFile img);
 
-    @PutMapping("/card/delete")
-    void cardDelete(Long id);
+    /**
+     * 카드 번호 유효성 검사 기능을 제공하는 메서드
+     *
+     * @param cardNumber 카드 번호 정보
+     * @return 카드 번호 유효성 검사 결과 - 카드 ID
+     */
+    @PostMapping(value = "/card/valid")
+    Long isCardNumberValid(@RequestBody CardRegisterRequestDTO cardNumber);
+
+    /**
+     * 카드 삭 제 기능을 제공하는 메서드
+     *
+     * @param id 삭제할 카드 ID
+     */
+    @PutMapping(value = "/card/delete")
+    void deleteCard(Long id);
+
+    /**
+     * 페이머니 생성 기능을 제공하는 메서드
+     *
+     * @param memberId 페이머니를 생성할 회원 ID
+     * @return 페이머니 생성 성공 여부
+     */
+    @PostMapping(value = "/pay-money")
+    boolean createPayMoney(Long memberId);
+
 }
