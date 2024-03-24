a4f4135b37086e1d46a7a5f7f706cf522ac81e91
고명성 <audtjd9080@naver.com>
2024-03-25 01:11:10 +0900

    ♻️ refactor: MemberRepository에 MemberRepositoryCustom을 확장하여 사용자 정의 쿼리 메소드 추가
    - Querydsl 를 지원하기 위해 MemberRepositoryCustom 을 MemberRepository 에 통합함.

--- be/common/src/main/java/com/ieum/common/repository/MemberRepository.java
+++ be/common/src/main/java/com/ieum/common/repository/MemberRepository.java
@@ -1,11 +1,13 @@
 package com.ieum.common.repository;
 
 import com.ieum.common.domain.Members;
+import com.ieum.common.repository.custom.MemberRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Members, Long> {
+public interface MemberRepository extends JpaRepository<Members, Long>, MemberRepositoryCustom {
 
     Members findByPhoneNumber(String phoneNumber);
+
     Members findByPhoneNumberAndPassword(String phoneNumber, String password);
-    // 여기에 추가적인 쿼리 메소드를 정의할 수 있습니다.
+
 }
