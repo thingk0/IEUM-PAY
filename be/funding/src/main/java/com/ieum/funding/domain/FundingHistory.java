1aec82179ca2e3cec5206beb67c841887cedc2a9
김범수 <kkkbs41@daum.net>
2024-03-25 15:29:10 +0900

    🚑 Hotfix: FundingHistory DTO import 추가
    Change-Id: I4738698e7105aa560218fa5222ffd9084fef9f22

--- be/funding/src/main/java/com/ieum/funding/domain/FundingHistory.java
+++ be/funding/src/main/java/com/ieum/funding/domain/FundingHistory.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
