b9135e6c1574004817050281ea088c867e239907
고명성 <audtjd9080@naver.com>
2024-03-25 22:12:09 +0900

    ✨ feat: 추가 예외 처리 기능 구현
    - 새로운 예외 클래스 처리 추가: InvalidPhoneNumberException, TokenOperationException, CookieOperationException 등 다양한 예외 상황에 대한 처리 로직 추가로 더욱 견고한 예외 관리 가능
    - PaymentServiceUnavailableException 등의 예외 처리를 통해 외부 서비스 연동 중 발생할 수 있는 오류에 대응
    - 각 예외 별로 적절한 HTTP 상태 코드와 함께 명확한 에러 메시지 반환을 통해 API 사용자가 오류의 원인을 쉽게 파악할 수 있도록 개선

--- be/common/src/main/java/com/ieum/common/handle/GlobalExceptionHandler.java
+++ be/common/src/main/java/com/ieum/common/handle/GlobalExceptionHandler.java
@@ -1,17 +1,27 @@
 package com.ieum.common.handle;
 
+import com.ieum.common.exception.cookie.CookieOperationException;
+import com.ieum.common.exception.feign.PaymentServiceUnavailableException;
+import com.ieum.common.exception.member.DuplicateNicknameChangeException;
 import com.ieum.common.exception.member.ExistingPhoneNumberException;
+import com.ieum.common.exception.member.InvalidPhoneNumberException;
+import com.ieum.common.exception.member.InvalidPrincipalTypeException;
+import com.ieum.common.exception.member.MemberNotFoundByIdException;
 import com.ieum.common.exception.member.MemberNotFoundException;
 import com.ieum.common.exception.member.PasswordMismatchException;
-import com.ieum.common.format.code.ErrorCode;
+import com.ieum.common.exception.token.TokenOperationException;
+import com.ieum.common.format.code.FailedCode;
 import com.ieum.common.format.response.ResponseTemplate;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -25,9 +35,9 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(RuntimeException.class)
     protected ResponseEntity<?> handle(RuntimeException e) {
-        log.error("RuntimeException = {}", e.getMessage());
         e.printStackTrace();
-        return response.error(ErrorCode.GLOBAL_UNEXPECTED_ERROR, HttpStatus.INTERNAL_SERVER_ERROR);
+        log.error("RuntimeException = {}", e.getMessage());
+        return response.error(FailedCode.RAISED_UNEXPECTED_ERROR);
     }
 
     @Override
@@ -35,26 +45,69 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                                                                   HttpHeaders headers,
                                                                   HttpStatus status,
                                                                   WebRequest request) {
-        log.error("MethodArgumentNotValidException = {}", e.getMessage());
+        log.error("ValidationException = {}", e.getMessage());
         return (ResponseEntity<Object>) response.fail(e.getBindingResult(), HttpStatus.BAD_REQUEST);
     }
 
+
     @ExceptionHandler(MemberNotFoundException.class)
     protected ResponseEntity<?> handle(MemberNotFoundException e) {
-        log.error("MemberNotFoundException = {}", e.getErrorCode().getMessage());
-        return response.error(e.getErrorCode(), HttpStatus.BAD_REQUEST);
+        log.error("MemberNotFoundException = {}", e.getFailedCode().getMessage());
+        return response.error(e.getFailedCode());
+    }
+
+    @ExceptionHandler(MemberNotFoundByIdException.class)
+    protected ResponseEntity<?> handle(MemberNotFoundByIdException e) {
+        log.error("MemberNotFoundByIdException = {}", e.getFailedCode().getMessage());
+        return response.error(e.getFailedCode());
     }
 
     @ExceptionHandler(PasswordMismatchException.class)
     protected ResponseEntity<?> handle(PasswordMismatchException e) {
-        log.error("PasswordMismatchException = {}", e.getErrorCode().getMessage());
-        return response.error(e.getErrorCode(), HttpStatus.BAD_REQUEST);
+        log.error("PasswordMismatchException = {}", e.getFailedCode().getMessage());
+        return response.error(e.getFailedCode());
     }
 
     @ExceptionHandler(ExistingPhoneNumberException.class)
     protected ResponseEntity<?> handle(ExistingPhoneNumberException e) {
-        log.error("ExistingPhoneNumberException = {}", e.getErrorCode().getMessage());
-        return response.error(e.getErrorCode(), HttpStatus.BAD_REQUEST);
+        log.error("ExistingPhoneNumberException = {}", e.getFailedCode().getMessage());
+        return response.error(e.getFailedCode());
+    }
+
+    @ExceptionHandler(InvalidPhoneNumberException.class)
+    protected ResponseEntity<?> handle(InvalidPhoneNumberException e) {
+        log.error("InvalidPhoneNumberException = {}", e.getFailedCode().getMessage());
+        return response.error(e.getFailedCode());
+    }
+
+    @ExceptionHandler(TokenOperationException.class)
+    protected ResponseEntity<?> handle(TokenOperationException e) {
+        log.error("TokenOperationException = {}", e.getFailedCode().getMessage());
+        return response.error(e.getFailedCode());
+    }
+
+    @ExceptionHandler(CookieOperationException.class)
+    protected ResponseEntity<?> handle(CookieOperationException e) {
+        log.error("CookieOperationException = {}", e.getFailedCode().getMessage());
+        return response.error(e.getFailedCode());
+    }
+
+    @ExceptionHandler(DuplicateNicknameChangeException.class)
+    protected ResponseEntity<?> handle(DuplicateNicknameChangeException e) {
+        log.error("DuplicateNicknameChangeException = {}", e.getFailedCode().getMessage());
+        return response.error(e.getFailedCode());
+    }
+
+    @ExceptionHandler(InvalidPrincipalTypeException.class)
+    protected ResponseEntity<?> handle(InvalidPrincipalTypeException e) {
+        log.error("InvalidPrincipalTypeException = {}", e.getFailedCode().getMessage());
+        return response.error(e.getFailedCode());
+    }
+
+    @ExceptionHandler(PaymentServiceUnavailableException.class)
+    protected ResponseEntity<?> handle(PaymentServiceUnavailableException e) {
+        log.error("PaymentServiceUnavailableException = {}", e.getFailedCode().getMessage());
+        return response.error(e.getFailedCode());
     }
 
 }
