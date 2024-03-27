99849a20d52fbb93aadfb734847de4dfebe796d1
이은규 <eung9702@gmail.com>
2024-03-27 17:52:45 +0900

    🚑 Hotfix: 주석
    Change-Id: Ifc80cc0fbff6218293bfd0a42b48cf6803ce6860

--- fe/web/pages/test2.tsx
+++ fe/web/pages/test2.tsx
@@ -3,30 +3,30 @@ import Html5QrcodeScanner from 'html5-qrcode';
 import { Html5QrcodeError } from 'html5-qrcode/esm/core';
 
 const ScannerComponent: React.FC = () => {
-  useEffect(() => {
-    const html5QrcodeScanner = new Html5QrcodeScanner(
-      'reader',
-      {
-        // Configuration options
-      },
-      false, // verbose option
-    );
+  //   useEffect(() => {
+  //     const html5QrcodeScanner = new Html5QrcodeScanner(
+  //       'reader',
+  //       {
+  //         // Configuration options
+  //       },
+  //       false, // verbose option
+  //     );
 
-    const onScanSuccess = (decodedText: string, decodedResult: any) => {
-      console.log(`Scan result: ${decodedText}`, decodedResult);
-      html5QrcodeScanner.clear();
-    };
+  //     const onScanSuccess = (decodedText: string, decodedResult: any) => {
+  //       console.log(`Scan result: ${decodedText}`, decodedResult);
+  //       html5QrcodeScanner.clear();
+  //     };
 
-    const onScanError = (errorMessage: Html5QrcodeError) => {
-      // handle on error condition, with error message
-    };
+  //     const onScanError = (errorMessage: Html5QrcodeError) => {
+  //       // handle on error condition, with error message
+  //     };
 
-    html5QrcodeScanner.render(onScanSuccess, onScanError);
+  //     html5QrcodeScanner.render(onScanSuccess, onScanError);
 
-    return () => {
-      html5QrcodeScanner.clear();
-    };
-  }, []);
+  //     return () => {
+  //       html5QrcodeScanner.clear();
+  //     };
+  //   }, []);
 
   return (
     <div id="reader">
