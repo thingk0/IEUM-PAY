343f9a87a7ac8acd221c54c1460905b34fbdc7e0
이은규 <eung9702@gmail.com>
2024-03-27 13:21:56 +0900

    ✨ feat: 결제 완료 페이지, LineBox 컴포넌트 완성 / 모금 완료 페이지 틀 제작
    Change-Id: I2a8f180fb61575e8e26e597bf646ea0823e2c84a

--- fe/web/pages/payment/PaymentComplete.tsx
+++ fe/web/pages/payment/PaymentComplete.tsx
@@ -1,10 +1,36 @@
 import PageTitleCenter from '@/components/PageTitleCenter';
 import styles from './paymentComplete.module.scss';
+import LineBox from '@/components/LineBox';
+import Button from '@/stories/Button';
+import { commaizeNumber } from '@toss/utils';
+import CompleteBadge from '@/public/pageBadges/CompleteBadge';
+import { useRouter } from 'next/router';
 
-export default function PaymentComplete() {
+interface PaymentComplete {
+  response: Object;
+}
+export default function CompletePage({}) {
+  const router = useRouter();
+  const goMain = () => {
+    router.push({
+      pathname: '/',
+    });
+  };
   return (
     <div className={styles.container}>
       <PageTitleCenter title={'결제 완료'} description={''} />
+      <div className={styles.badge}>
+        <CompleteBadge />
+      </div>
+      <div className={styles.LineBoxes}>
+        <LineBox main={`구매 결제 : ${commaizeNumber(5600)}원`} sub={'쿠팡'} />
+        <LineBox main={'잔돈 기부 : 400원'} sub={'BTC 아동센터'} />
+      </div>
+      <div className={styles.btnContainer}>
+        <Button primary size="thick" onClick={goMain}>
+          확인
+        </Button>
+      </div>
     </div>
   );
 }
