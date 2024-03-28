4a1eea16ce6a32eda5e54c1c5dcdbbb0f20f0244
이은규 <eung9702@gmail.com>
2024-03-28 13:12:20 +0900

    📝 doc: qr스캔 카메라 후면 고정 시도
    Change-Id: I3f191aa71bc77e71ec51e5a7409e455d37cf48c1

--- fe/web/pages/payment/qrscan.tsx
+++ fe/web/pages/payment/qrscan.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Html5QrcodePlugin from './QRscanner';
 import Html5QrcodeError from './QRscanner';
 import { useRouter } from 'next/router';
+import styles from './qrscan.module.scss';
 
 interface AppProps {
   // props의 타입을 정의해줍니다.
@@ -9,8 +10,8 @@ interface AppProps {
 
 const QRscan: React.FC<AppProps> = (props) => {
   const onNewScanResult = (decodedText: any, decodedResult: any) => {
-    console.log(decodedText);
-    console.log(decodedResult);
+    console.log('t', decodedText);
+    console.log('r', decodedResult);
     setQrResult(decodedText);
   };
 
@@ -23,17 +24,18 @@ const QRscan: React.FC<AppProps> = (props) => {
   }
   return (
     <div className="App">
-      <p>asdasd</p>
-
-      <Html5QrcodePlugin
-        fps={10}
-        qrbox={{ width: 250, height: 250 }} // qrbox는 너비와 높이를 객체로 전달해야 합니다.
-        disableFlip={false}
-        qrCodeSuccessCallback={onNewScanResult}
-        qrCodeErrorCallback={(error: typeof Html5QrcodeError) =>
-          console.error(error)
-        } // 오류 콜백도 추가해줍니다.
-      />
+      <h1>QR 스캔</h1>
+      <div className={styles.qr}>
+        <Html5QrcodePlugin
+          fps={10}
+          qrbox={{ width: 250, height: 250 }} // qrbox는 너비와 높이를 객체로 전달해야 합니다.
+          disableFlip={false}
+          qrCodeSuccessCallback={onNewScanResult}
+          qrCodeErrorCallback={(error: typeof Html5QrcodeError) =>
+            console.error(error)
+          } // 오류 콜백도 추가해줍니다.
+        />
+      </div>
     </div>
   );
 };
