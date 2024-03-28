8eb5d0c8325cd871e4b4d4b37a17df90e1afac9d
김예지 <fpssj@naver.com>
2024-03-28 13:35:22 +0900

    ✨ feat: 영수증 공유하기 누르면 이미지 다운로드 되는 기능 추가
    Change-Id: Ie47644623a5d57818380b2398cdc0995f4cb72da

--- fe/web/pages/history/[receiptId]/index.tsx
+++ fe/web/pages/history/[receiptId]/index.tsx
@@ -3,25 +3,42 @@ import classes from './receipt.module.scss';
 import PageTitleCenter from '@/components/PageTitleCenter';
 import CustomDashedBorder from '@/components/CustomDashedBorder';
 import { commaizeNumber } from '@toss/utils';
+import { useRef } from 'react';
+import html2canvas from 'html2canvas';
+import { useRouter } from 'next/router';
+const data = {
+  fundingId: 1,
+  fundingTitle: 'btc 아동센터 아이들 20명에게',
+  facilityName: '인천',
+  historyDate: '2023-03-04 01:11:11',
+  nickname: '기부니',
+  donationAmount: 500,
+  fundingSummary: '이것저것 외 14개',
+};
 function ReceiptElement({ children }: { children: React.ReactNode }) {
   return <div className={classes.element}>{children}</div>;
 }
 
 export default function ReceiptPage() {
-  const data = {
-    fundingId: 1,
-    fundingTitle: 'btc 아동센터 아이들 20명에게',
-    facilityName: '인천',
-    historyDate: '2023-03-04 01:11:11',
-    nickname: '기부니',
-    donationAmount: 500,
-    fundingSummary: '이것저것 외 14개',
-  };
+  const receiptRef = useRef<HTMLDivElement>(null);
+  const router = useRouter();
+
+  function handleClick() {
+    if (receiptRef.current) {
+      html2canvas(receiptRef.current).then(function (canvas) {
+        const link = document.createElement('a');
+        link.download = 'receipt.png';
+        link.href = canvas.toDataURL('image/png');
+        link.click();
+      });
+    }
+  }
+
   return (
     <>
       <PageTitleCenter title={''} description={''}></PageTitleCenter>
       <main className={classes.container}>
-        <div className={classes.receipt}>
+        <div className={classes.receipt} ref={receiptRef}>
           <h1>이음페이 기부 영수증</h1>
           <CustomDashedBorder />
           <h2>{data.fundingTitle}</h2>
@@ -49,7 +66,9 @@ export default function ReceiptPage() {
             <span>{commaizeNumber(data.donationAmount)}원</span>
           </ReceiptElement>
         </div>
-        <Button primary>영수증 공유하기</Button>
+        <Button primary onClick={handleClick}>
+          영수증 공유하기
+        </Button>
       </main>
     </>
   );
