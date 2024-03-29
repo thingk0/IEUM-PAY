a1234dfea71b69fd35565ce5d96ad91eb4368d1e
고명성 <audtjd9080@naver.com>
2024-03-29 20:19:52 +0900

    🔨 refactor: PayService 및 PaymentService 클래스 내 기능 확장 및 개선
    - PayService 에서는 카드 등록, 결제 처리, 기부 내역 조회 등의 기능을 확장하고 세부적으로 구현하여, 결제 관련 다양한 요구 사항을 충족시킬 수 있도록 개선.
    - PaymentService 에서는 결제 프로세스 처리 로직을 추가하고, 결제 비밀번호 확인 및 변경, 결제 내역 조회 기능을 보완하여 사용자의 결제 경험을 개선.

--- be/common/src/main/java/com/ieum/common/service/PayService.java
+++ be/common/src/main/java/com/ieum/common/service/PayService.java
@@ -24,124 +24,126 @@ public class PayService {
 
     public Long cardRegister(Long memberId, String cardNumber, String cardNickname) {
         return payFeignClient.isCardNumberValid(CardRegisterRequestDTO.builder()
-                .memberId(memberId)
-                .cardNumber(cardNumber)
-                .cardNickname(cardNickname)
-                .build());
+                                                                      .memberId(memberId)
+                                                                      .cardNumber(cardNumber)
+                                                                      .cardNickname(cardNickname)
+                                                                      .build());
     }
 
-    public boolean updateCard(Long memberId ,Long cardId) {
-        return payFeignClient.deleteCard( CardValidRequestDTO.builder()
-                .memberId(memberId)
-                .registeredCardId(cardId)
-                .build());
+    public boolean updateCard(Long memberId, Long cardId) {
+        return payFeignClient.deleteCard(CardValidRequestDTO.builder()
+                                                            .memberId(memberId)
+                                                            .registeredCardId(cardId)
+                                                            .build());
     }
 
     public MainSummaryResponseDTO getMainPageInfo(Long memberId) {
         return payFeignClient.getMainPageInfo(memberId);
     }
 
-    public boolean createPaymoney(Long memberId, String payPassword){
+    public boolean createPaymoney(Long memberId, String payPassword) {
         return payFeignClient.createPayMoneyRegister(RegisterRequestDTO.builder()
-                        .memberId(memberId)
-                        .paymentPassword(payPassword)
-                        .build());
+                                                                       .memberId(memberId)
+                                                                       .paymentPassword(payPassword)
+                                                                       .build());
     }
 
-    public boolean updatePayPassword(Long memberId, String paymentPassword){
+    public boolean updatePayPassword(Long memberId, String paymentPassword) {
         return payFeignClient.updatePayPassword(MemberPayPasswordRequestDTO.builder()
-                .memberId(memberId)
-                .paymentPassword(paymentPassword)
-                .build());
+                                                                           .memberId(memberId)
+                                                                           .paymentPassword(paymentPassword)
+                                                                           .build());
     }
 
-    public FundingDonationResultResponseDTO getDonationHistory(Long historyId){
-        return payFeignClient.getDonationHistory(historyId);
+    public FundingDonationResultResponseDTO getDonationHistory(Long historyId) {
+        return payFeignClient.getDonationHistory(historyId).orElseThrow();
     }
 
-    public Long directDonation(Long memberId, Long fundingId, int donationAmount, Long cardId){
+    public Long directDonation(Long memberId, Long fundingId, int donationAmount, Long cardId) {
         return payFeignClient.directDonation(FundingDonationRequestDTO.builder()
-                .memberId(memberId)
-                .fundingId(fundingId)
-                .donationAmount(donationAmount)
-                .cardId(cardId)
-                .build());
+                                                                      .memberId(memberId)
+                                                                      .fundingId(fundingId)
+                                                                      .donationAmount(donationAmount)
+                                                                      .cardId(cardId)
+                                                                      .build());
     }
 
-    public List<HistoryResponseDTO> getHistoryList(Long memberId){
+    public List<HistoryResponseDTO> getHistoryList(Long memberId) {
         return payFeignClient.history(memberId);
     }
 
-    public int HowManyCharge(Long memberId, int tradeAmount){
+    public int HowManyCharge(Long memberId, int tradeAmount) {
         return payFeignClient.chargeMoney(PaymentChargeMoney.builder()
-                .memberId(memberId)
-                .paymentMoney(tradeAmount)
-                .build());
+                                                            .memberId(memberId)
+                                                            .paymentMoney(tradeAmount)
+                                                            .build());
     }
 
-    public Long payment(Long memberId,Long storeId, Long fundingId, Long cardId, int amount, int donationAmount,
-                        int chargeAmount){
+    public Long payment(Long memberId, Long storeId, Long fundingId, Long cardId,
+                        int amount, int donationAmount, int chargeAmount) {
+
         return payFeignClient.payment(PaymentRequestDTO.builder()
-                .memberId(memberId)
-                .storeId(storeId)
-                .fundingId(fundingId)
-                .cardId(cardId)
-                .amount(amount)
-                .donationAmount(donationAmount)
-                .chargeAmount(chargeAmount)
-                .build());
+                                                       .memberId(memberId)
+                                                       .storeId(storeId)
+                                                       .fundingId(fundingId)
+                                                       .cardId(cardId)
+                                                       .amount(amount)
+                                                       .donationAmount(donationAmount)
+                                                       .chargeAmount(chargeAmount)
+                                                       .build());
     }
 
-    public String getStoreName(Long storeId){
+    public String getStoreName(Long storeId) {
         return payFeignClient.getStoreName(storeId);
     }
 
-    public PaymentHistoryPayResponseDTO getPaymentHistory(Long memberId, Long historyId){
+    public PaymentHistoryPayResponseDTO getPaymentHistory(Long memberId, Long historyId) {
         return payFeignClient.getPaymentHistory(historyId);
     }
 
-    public int nowMyPaymoney(Long memberId){
+    public int nowMyPaymoney(Long memberId) {
         return payFeignClient.myPaymoney(memberId);
     }
 
     public Long remmitancePaymoney(Long member, String memberName, Long receiverId,
-                                   String receiverName, int amount, Long cardId){
+                                   String receiverName, int amount, Long cardId) {
         return payFeignClient.remittancePaymoney(RemittanceRequestDTO.builder()
-                .senderId(member)
-                .senderName(memberName)
-                .receiverId(receiverId)
-                .receiverName(receiverName)
-                .amount(amount)
-                .cardId(cardId)
-                .build());
+                                                                     .senderId(member)
+                                                                     .senderName(memberName)
+                                                                     .receiverId(receiverId)
+                                                                     .receiverName(receiverName)
+                                                                     .amount(amount)
+                                                                     .cardId(cardId)
+                                                                     .build());
     }
 
     public Long remmitanceAccount(Long memberId, String memberName, String receiverName,
-                                  int amount, Long cardId){
+                                  int amount, Long cardId) {
         return payFeignClient.remittanceAccount(RemittanceAccountRequestDTO.builder()
-                .senderId(memberId)
-                .senderName(memberName)
-                .receiverName(receiverName)
-                .amount(amount)
-                .cardId(cardId)
-                .build());
+                                                                           .senderId(memberId)
+                                                                           .senderName(memberName)
+                                                                           .receiverName(receiverName)
+                                                                           .amount(amount)
+                                                                           .cardId(cardId)
+                                                                           .build());
     }
 
-    public DonationReceiptResponseDTO getDonationReceiptInfo(Long memberId, Long historyId){
-        return payFeignClient.donationReceipt(memberId,historyId);
+    public DonationReceiptResponseDTO getDonationReceiptInfo(Long memberId, Long historyId) {
+        return payFeignClient.donationReceipt(memberId, historyId);
     }
 
-    public boolean checkMyCard(Long memberId,Long registeredCardId){
+    public boolean checkMyCard(Long memberId, Long registeredCardId) {
         return payFeignClient.isMine(MyCardCheckRequestDTO.builder()
-                .memberId(memberId)
-                .registeredCardId(registeredCardId)
-                .build());
-    }
-    public RemittanceHistoryResponseDTO getRemittanceHistory(Long memberId, Long historyId){
-        return payFeignClient.getRemittanceHistory(memberId,historyId);
+                                                          .memberId(memberId)
+                                                          .registeredCardId(registeredCardId)
+                                                          .build());
     }
 
-    public String getCardName(Long registeredCardId){
+    public RemittanceHistoryResponseDTO getRemittanceHistory(Long memberId, Long historyId) {
+        return payFeignClient.getRemittanceHistory(memberId, historyId);
+    }
+
+    public String getCardName(Long registeredCardId) {
         return payFeignClient.getCardName(registeredCardId);
     }
 
