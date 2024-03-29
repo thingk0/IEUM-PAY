a7deadd132b9188d4ae34bb4f690b1e9b3e9c5de
박수형 <nastorond@kakao.com>
2024-03-29 13:29:52 +0900

    ✨ feat: 티어리스트 추가
    Change-Id: I5cc7edd4bd5cf7c0928dacb2129f9a7b1ef9a8d9

--- fe/web/pages/my-page/tear-list.tsx
+++ fe/web/pages/my-page/tear-list.tsx
@@ -1,21 +1,24 @@
 import { badges } from '@/components/icons/LevelBadges';
+import useUserStore from '@/stores/user-store';
 import HeaderMain from '@/stories/HeaderMain';
 import TabBar from '@/stories/TabBar';
 import styles from '@/styles/tearListPage.module.scss';
+import { commaizeNumber } from '@toss/utils';
+import { GetServerSideProps } from 'next';
 
-export default function TearList() {
+function TearList({ gradeCode }: { gradeCode: string }) {
   const setBedge = (level: string) => {
     const BadgeComponent = badges[level];
     return <BadgeComponent />;
   };
   const levels = [
-    'GR001',
-    'GR002',
-    'GR003',
-    'GR004',
-    'GR005',
-    'GR006',
-    'GR007',
+    { grade: 'GR001', name: '씨앗', amount: 0, cnt: 0 },
+    { grade: 'GR002', name: '새싹', amount: 100, cnt: 1 },
+    { grade: 'GR003', name: '떡잎', amount: 5000, cnt: 10 },
+    { grade: 'GR004', name: '묘목', amount: 30000, cnt: 30 },
+    { grade: 'GR005', name: '나무', amount: 100000, cnt: 50 },
+    { grade: 'GR006', name: '열매', amount: 1000000, cnt: 70 },
+    { grade: 'GR007', name: '이음', amount: 10000000, cnt: 100 },
   ];
 
   return (
@@ -23,14 +26,26 @@ export default function TearList() {
       <HeaderMain />
       <div className={styles.table}>
         <div className={styles.header}>
-          <div>등급</div>
-          <div>누적 기부 액</div>
-          <div>기부 횟수</div>
+          <div className={styles.lavel}>등급</div>
+          <div className={styles.lavel}>누적 기부 액</div>
+          <div className={styles.lavel}>기부 횟수</div>
         </div>
         <hr />
         <ol>
           {levels.map((level) => (
-            <li>{setBedge(level)}</li>
+            <>
+              <li className={gradeCode == level.grade ? styles.box : styles.li}>
+                <div className={styles.grade}>
+                  {setBedge(level.grade)}
+                  <p>{level.name}</p>
+                </div>
+                <div className={styles.amount}>
+                  {commaizeNumber(level.amount)}원
+                </div>
+                <div className={styles.cnt}>{level.cnt}</div>
+              </li>
+              <hr />
+            </>
           ))}
         </ol>
       </div>
@@ -38,3 +53,10 @@ export default function TearList() {
     </>
   );
 }
+export default TearList;
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  // query 객체 사용
+  return {
+    props: { gradeCode: query.gradeCode },
+  };
+};
