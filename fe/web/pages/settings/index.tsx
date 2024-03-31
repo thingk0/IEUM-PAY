ca86e5cd6bcd2f4f23e33dae5a6b604e0e7f5ef2
박수형 <nastorond@kakao.com>
2024-03-31 21:17:05 +0900

    ✨ feat: policy page 추가
    Change-Id: Iab0a083e92013ff58a6f43d7475d161a3ec8d435

--- fe/web/pages/settings/index.tsx
+++ fe/web/pages/settings/index.tsx
@@ -2,14 +2,23 @@ import PageTitleLeft from '@/components/PageTitleLeft';
 import styles from './settings.module.scss';
 import SettingsMenu from '@/components/settingsMenu';
 import ChevronRightIcon from '@/components/icons/ChevronRightIcon';
-import HalfButton from '@/components/HalfButton';
 import HeaderMain from '@/stories/HeaderMain';
 import TabBar from '@/stories/TabBar';
 import { eraseCookie } from '@/utils/cookie';
 import { useRouter } from 'next/router';
+import {
+  Modal,
+  ModalContent,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+} from '@nextui-org/react';
+import Button from '@/stories/Button';
 
 export default function Settings() {
   const router = useRouter();
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
   return (
     <>
       <HeaderMain />
@@ -24,19 +33,56 @@ export default function Settings() {
         </div>
         <div className={styles.menuBox}>
           <SettingsMenu>비밀번호 변경</SettingsMenu>
-          <SettingsMenu>약관 및 정책</SettingsMenu>
           <SettingsMenu
             onClick={() => {
-              eraseCookie('access_token');
-              router.push('/user');
+              router.push('/settings/policy');
             }}
           >
-            로그아웃
+            약관 및 정책
           </SettingsMenu>
+          <SettingsMenu onClick={() => onOpen()}>로그아웃</SettingsMenu>
           <SettingsMenu>탈퇴하기</SettingsMenu>
         </div>
       </div>
       <TabBar selected={'none'} />
+      <Modal
+        className={styles.modalComp}
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalBody className={styles.modalContainer}>
+                <div>
+                  <p>정말 로그아웃 할까요?</p>
+                </div>
+              </ModalBody>
+              <ModalFooter className={styles.modalFooter}>
+                <Button
+                  size="thin"
+                  onClick={() => {
+                    onClose;
+                  }}
+                >
+                  취소
+                </Button>
+                <Button
+                  primary
+                  size="thin"
+                  onClick={() => {
+                    eraseCookie('access_token');
+                    router.push('/user');
+                    onClose;
+                  }}
+                >
+                  확인
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
     </>
   );
 }
