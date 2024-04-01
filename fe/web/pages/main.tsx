dc979ef4d0766066192bd23807db0ac7000f6c7f
이은규 <eung9702@gmail.com>
2024-04-01 16:26:28 +0900

    ✨ feat: 메인 페이지 카드사 연동 완료
    Change-Id: Ib5f005949f4919cb4b2bfad7d5632b90aa110b0d

--- fe/web/pages/main.tsx
+++ fe/web/pages/main.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 import { Inter } from 'next/font/google';
 import mainStyles from './main.module.scss';
+import bank from './bank.module.scss';
 
 import TabBar from '@/stories/TabBar';
 import Money from '@/components/Money';
@@ -9,9 +10,10 @@ import HeaderMain from '@/stories/HeaderMain';
 import Button from '@/stories/Button';
 import { useRouter } from 'next/router';
 import MainPageDropdown from '@/components/MainPageDropdown';
-import { getBalance } from '@/api/paymentAxios';
-import useUserStore from '@/stores/user-store';
+// import { getBalance } from '@/api/paymentAxios';
+// import useUserStore from '@/stores/user-store';
 import { getMainData } from '@/api/userAxois';
+import { useQuery } from '@tanstack/react-query';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -28,14 +30,28 @@ interface infoType {
   paymentAmount: number;
   totalDonation: number;
 }
+interface logoUrlType {
+  신한카드: string;
+  KB국민카드: string;
+  광주은행: string;
+  삼성카드: string;
+  수협은행: string;
+  NH농협카드: string;
+  BC카드: string;
+  우리카드: string;
+  롯데카드: string;
+  현대카드: string;
+  하나카드: string;
+  전북은행: string;
+  제주은행: string;
+  씨티카드: string;
+  이음페이: string;
+}
 
 export default function Home() {
   const router = useRouter();
-  const [balance, setMainBalance] = useState<number>();
-  const { setBalance } = useUserStore();
-  const goFund = () => {
-    router.push('/fundraising');
-  };
+  const [cardState, setCardState] = useState<number[]>([]);
+  const [prevCardState, setPrevCardState] = useState<number[]>([]);
   const [info, setInfo] = useState<infoType>({
     cardList: [
       {
@@ -51,22 +67,44 @@ export default function Home() {
     totalDonation: 1843490,
   });
 
+  const logoUrl: logoUrlType = {
+    신한카드: 'https://www.shinhancard.com/pconts/images/shcard/ci-shinhan.png',
+    KB국민카드: 'https://img1.kbcard.com/LT/images_r/common/kbcard_Logo_v3.png',
+    광주은행:
+      'https://i.namu.wiki/i/nvBUwz04V7Nku4grdLmnwCPlpM_Feo5k7TZeL_ulRUT6Ic1qIn_R8WuFkyO9xA5BVWzL8iF2tJXxLtOgnaiLHg.svg',
+    삼성카드: 'https://www.inicis.com/apple/popup/img/card_ss.png',
+    수협은행:
+      'https://i.namu.wiki/i/aGBgqIBemO3l45_2-CL6CMxF8DNXNZHRwkchw_b-byNVPmitL1UC6Ydv-9-6MVQdJWCttJvFwGdiIWEaBwp1ww.svg',
+    NH농협카드: 'https://card.nonghyup.com/images/IP/cc/layout/n_logo.png',
+    BC카드:
+      'https://i.namu.wiki/i/8C2vbjyHD6CuR-QXhE5rFxRUDbANfr5OrSNxW_24bihqOYM1VUxR84cwTtXZ_3RE7x4qqb2Ald_YDBUiiF9Wag.svg',
+    우리카드:
+      'https://biz.chosun.com/resizer/YR43ZY7Vjg3tSGXvdOD4T0SLayM=/616x0/smart/cloudfront-ap-northeast-1.images.arcpublishing.com/chosunbiz/QVXYKQKAHJEGJFI2SNI3HZTKZM.png',
+    롯데카드:
+      'https://i.namu.wiki/i/KPgfJH2zZxOvGgO_6QU-XUZptxNfiZXyCMoeMu_p1PKM2WT_XG2j-C84wft7fE02rQYzsadt1dh8GdobWwq7dw.svg',
+    현대카드:
+      'https://upload.wikimedia.org/wikipedia/commons/f/f0/Hyundai_card_CI_new.png',
+    하나카드:
+      'https://i.namu.wiki/i/8JEi0JqCKkxGgCNptgwW8idqtPlZzc7RnuD4cbojlt1D45iybb0qQwQtXnJECnCG0ZUdPqK_pog0DQjNYVv1Mg.svg',
+    전북은행:
+      'https://i.namu.wiki/i/cxJG39QcJ8i5P4dznfK-ELik8uaNIWnvTrLv40_xe9JT2ENcSFfjJ1PGutPPU0JyYnxW5LVoZJYjUtFfjgHF6g.svg',
+    제주은행:
+      'https://www.shinhangroup.com/kr/asset/images/introduce/shinhan_CI_kr_2022-08.png',
+    씨티카드: 'https://rasta.co.kr/web/upload/img/card/card_ct.png',
+    이음페이: '/longLogo.svg',
+  };
+
+  const goFund = () => {
+    router.push('/fundraising');
+  };
+
   useEffect(() => {
     async function fetchBalance() {
-      let { data } = await getBalance();
-      let balance = data.data;
-      setMainBalance(balance);
-      setBalance(balance);
       const mainData = await getMainData();
       mainData != undefined ? setInfo(mainData.data) : '';
-      console.log(mainData.data);
     }
     fetchBalance();
   }, []);
-
-  // 카드 관련
-  const [cardState, setCardState] = useState<number[]>([]);
-  const [prevCardState, setPrevCardState] = useState<number[]>([]);
 
   useEffect(() => {
     const initializeCards = () => {
@@ -84,6 +122,12 @@ export default function Home() {
     setPrevCardState([...cardState]);
     setCardState(updatedCardState);
   };
+
+  const { data, error, isError, isLoading, refetch } = useQuery({
+    queryKey: ['get-main'],
+    queryFn: getMainData,
+  });
+
   return (
     <>
       <HeaderMain />
@@ -91,21 +135,42 @@ export default function Home() {
         <MainPageDropdown></MainPageDropdown>
         <div className={mainStyles.cardsContainer}>
           <div className={mainStyles.cardStack} onClick={nextCard}>
-            {cardState.map((card, index) => (
-              <div
-                key={index}
-                className={`${mainStyles.card} ${mainStyles['card' + card]} ${mainStyles['bank' + index]}`}
-              >
-                {/* {String.fromCharCode(64 + card)} */}
-                {index}
-              </div>
-            ))}
+            {isLoading
+              ? null
+              : cardState.map((card, index) => (
+                  <div
+                    key={index}
+                    className={`${mainStyles.card} ${mainStyles['card' + card]} ${bank[info.cardList[index].cardIssuer]}`}
+                  >
+                    {/* {index} */}
+                    <div className={mainStyles.logoBox}>
+                      <img
+                        src={logoUrl[info.cardList[index].cardIssuer]}
+                        alt="bankLogo"
+                        className={mainStyles.logo}
+                      />
+                    </div>
+                    <div className={mainStyles.cardBottom}>
+                      <div className={mainStyles.nicknameContainer}>
+                        {info.cardList[index].cardNickname}
+                      </div>
+                    </div>
+                  </div>
+                ))}
           </div>
         </div>
         <div className={mainStyles.moneyContainer}>
-          <Money text={'이음페이머니'} amount={balance} onClick={goFund} />
+          <Money
+            text={'이음페이머니'}
+            amount={info.paymentAmount}
+            onClick={goFund}
+          />
           <hr />
-          <Money text={'기부총액'} amount={'24,200'} onClick={goFund} />
+          <Money
+            text={'기부총액'}
+            amount={info.totalDonation}
+            onClick={goFund}
+          />
         </div>
       </main>
       <div className={mainStyles.btnContainer}>
