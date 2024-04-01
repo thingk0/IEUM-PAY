74e0d8d18f70b31680dc57d4386446830f5d4444
김보경 <love2491193@naver.com>
2024-04-01 12:59:59 +0900

    ✨ feat: 카드 번호 앞 8자리로 회사 정보 get
    Change-Id: Ib4c7c33291af9c716d5245694f86aea002da5503

--- be/pay/src/main/java/com/ieum/pay/service/CardService.java
+++ be/pay/src/main/java/com/ieum/pay/service/CardService.java
@@ -4,6 +4,7 @@ import com.ieum.pay.domain.Cards;
 import com.ieum.pay.domain.RegisteredCards;
 import com.ieum.pay.repository.CardRepository;
 import com.ieum.pay.repository.RegisteredCardRepository;
+import com.ieum.pay.response.CardDetailResponseDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,15 +18,15 @@ public class CardService {
     private final RegisteredCardRepository registeredCardRepository;
 
     public Cards findCard(String cardNumber){
-        int cardNumFour = Integer.parseInt(cardNumber.substring(0, 4));
-        int cardNumSix = Integer.parseInt(cardNumber.substring(0,6));
+        int cardNumFour = Integer.parseInt(cardNumber.substring(0, 6));
+        int cardNumSix = Integer.parseInt(cardNumber.substring(0,8));
         List<Cards> cardsList = cardRepository.findByCardNumber(cardNumFour, cardNumSix);
 
         Cards cards = new Cards(4000L,"이음페이","이음카드",0,0);
 
         for (Cards c : cardsList) {
             int bin = c.getCardBin();
-            if(bin < 10000){
+            if(bin < 1000000){
                 return c;
             }
             if(cardNumSix == bin){
@@ -46,5 +47,17 @@ public class CardService {
     public String getCardName(Long id) {
         RegisteredCards card = registeredCardRepository.findByRegisteredCardId(id);
         return card.getCardNickname();
+    }
+
+    public CardDetailResponseDTO getCardCompany(String number) {
+        List<Cards> cardsList = cardRepository.findByCardNumber(
+                Integer.parseInt(number.substring(0,6)), Integer.parseInt(number.substring(0,8)));
+        if(cardsList.size() == 0 )
+            return CardDetailResponseDTO.builder().build();
+        Cards cards = cardsList.get(0);
+        return CardDetailResponseDTO.builder()
+                .issuer(cards.getIssuer())
+                .product(cards.getCardProduct())
+                .build();
     }
 }
