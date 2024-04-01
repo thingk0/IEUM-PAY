babd42b3feef419480c468844c0d5cde0af21891
고명성 <audtjd9080@naver.com>
2024-04-02 08:51:52 +0900

    ✨ feat: 성공 응답 코드 추가
    - 액세스 토큰 갱신, 기부 결과 조회, 사용 내역 조회 등 다양한 성공 상황에 대한 코드 추가

--- be/common/src/main/java/com/ieum/common/format/code/SuccessCode.java
+++ be/common/src/main/java/com/ieum/common/format/code/SuccessCode.java
@@ -31,9 +31,49 @@ public enum SuccessCode {
 
     /* 인증 */
     AUTHENTICATION_CODE_ISSUED(HttpStatus.OK, "본인 확인용 인증 코드가 성공적으로 발급되었습니다."),
+    ACCESS_TOKEN_RENEWED(HttpStatus.OK, "액세스 토큰 갱신이 성공적으로 완료되었습니다."),
 
     /* 기부 */
     DIRECT_DONATION_RESULT_SUCCESS(HttpStatus.OK, "직접 기부 결과 조회가 성공적으로 완료되었습니다."),
+
+    HISTORY_LIST_FETCHED(HttpStatus.OK, "사용 내역 조회가 성공적으로 완료되었습니다."),
+    REMITTANCE_HISTORY_FETCHED(HttpStatus.OK, "송금 내역 조회가 성공적으로 완료되었습니다."),
+    PAYMONEY_REMITTANCE_SUCCESS(HttpStatus.OK, "페이머니 송금이 성공적으로 완료되었습니다."),
+
+    /* 페이먼트 */
+    PAYMONEY_BALANCE_FETCHED(HttpStatus.OK, "페이먼트 잔액 조회가 성공적으로 완료되었습니다."),
+    MAIN_CARD_SETTING_SUCCESSFUL(HttpStatus.OK, "메인 카드 설정이 성공적으로 완료되었습니다."),
+    CARD_OCR_SUCCESS(HttpStatus.OK, "카드 OCR 처리가 성공적으로 완료되었습니다."),
+    CARD_UPDATE_SUCCESSFUL(HttpStatus.OK, "카드 정보 업데이트가 성공적으로 완료되었습니다."),
+
+    /* 펀딩 */
+    FUNDING_DETAIL_FETCHED(HttpStatus.OK, "펀딩 상세 정보 조회가 성공적으로 완료되었습니다."),
+    FUNDING_ONGOING_LIST_FETCHED(HttpStatus.OK, "진행 중인 펀딩 목록 조회가 성공적으로 완료되었습니다."),
+    FUNDING_COMPLETE_LIST_FETCHED(HttpStatus.OK, "완료된 펀딩 목록 조회가 성공적으로 완료되었습니다."),
+    FUNDING_PARTICIPATION_LIST_FETCHED(HttpStatus.OK, "참여했던 펀딩 목록 조회가 성공적으로 완료되었습니다."),
+    FUNDING_LINKUP_SUCCESS(HttpStatus.OK, "펀딩 연계가 성공적으로 완료되었습니다."),
+    FUNDING_UNLINK_SUCCESS(HttpStatus.OK, "펀딩 연계 해제가 성공적으로 완료되었습니다."),
+    FUNDING_LINK_RESULT_FETCHED(HttpStatus.OK, "펀딩 연동 결과 조회가 성공적으로 완료되었습니다."),
+    DIRECT_DONATION_SUCCESS(HttpStatus.OK, "직접 기부가 성공적으로 완료되었습니다."),
+    DIRECTLY_FUNDING_INFO_FETCHED(HttpStatus.OK, "직접기부 결제 정보 조회가 성공적으로 완료되었습니다."),
+    RECEIPT_INFO_FETCHED(HttpStatus.OK, "영수증 정보 조회가 성공적으로 완료되었습니다."),
+    CURRENT_INFO_FETCHED(HttpStatus.OK, "현재 정보 (기부 관련) 조회가 성공적으로 완료되었습니다."),
+
+    /* 메인 페이지 */
+    MAIN_PAGE_SUMMARY_FETCHED(HttpStatus.OK, "메인 페이지 정보 조회가 성공적으로 완료되었습니다."),
+
+    /* 카드 관련 */
+    CARD_OCR_PROCESS_SUCCESSFUL(HttpStatus.OK, "카드 OCR 처리가 성공적으로 완료되었습니다."),
+
+    /* 회원 정보 */
+    MEMBER_SUMMARY_FETCHED(HttpStatus.OK, "회원 요약 정보 조회가 성공적으로 완료되었습니다."),
+
+    /* 결제 처리 */
+    PAYMENT_PROCESS_SUCCESSFUL(HttpStatus.OK, "결제 처리가 성공적으로 완료되었습니다."),
+    PAYMENT_PASSWORD_VERIFICATION_SUCCESS(HttpStatus.OK, "결제 비밀번호 인증이 성공적으로 완료되었습니다."),
+    PAYMENT_PASSWORD_UPDATE_SUCCESS(HttpStatus.OK, "결제 비밀번호 변경이 성공적으로 완료되었습니다."),
+    PAYMENT_HISTORY_FETCHED(HttpStatus.OK, "결제 내역 조회가 성공적으로 완료되었습니다."),
+    PAYMENT_INFO_FETCHED(HttpStatus.OK, "결제 정보 조회가 성공적으로 완료되었습니다."),
 
     ;
 
