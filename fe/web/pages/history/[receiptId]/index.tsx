9ab9f064765e4906d79dc5044a444a45973fd61e
김예지 <fpssj@naver.com>
2024-04-01 11:17:52 +0900

    🚑 Hotfix: response data undefined일때 처리해서 빌드 오류 해결
    Change-Id: I7a4bfd5808a5a5556c9644678bf9a059c0ec177f

--- fe/web/pages/history/[receiptId]/index.tsx
+++ fe/web/pages/history/[receiptId]/index.tsx
@@ -8,6 +8,7 @@ import html2canvas from 'html2canvas';
 import { useRouter } from 'next/router';
 import { useQuery } from '@tanstack/react-query';
 import { getReceipt } from '@/api/historyAxios';
+import FetchError from '@/components/layouts/FetchError';
 
 function ReceiptElement({ children }: { children: React.ReactNode }) {
   return <div className={classes.element}>{children}</div>;
@@ -16,7 +17,7 @@ function ReceiptElement({ children }: { children: React.ReactNode }) {
 export default function ReceiptPage() {
   const router = useRouter();
   const receiptId = router.query.receiptId;
-  const { data } = useQuery({
+  const { data, isError, isLoading, refetch } = useQuery({
     queryKey: [receiptId],
     queryFn: () => getReceipt(receiptId),
   });
@@ -32,43 +33,46 @@ export default function ReceiptPage() {
       });
     }
   }
-
-  return (
-    <>
-      <PageTitleCenter title={''} description={''}></PageTitleCenter>
-      <main className={classes.container}>
-        <div className={classes.receipt} ref={receiptRef}>
-          <h1>이음페이 기부 영수증</h1>
-          <CustomDashedBorder />
-          <h2>{data.fundingTitle}</h2>
-          <CustomDashedBorder />
-          <ReceiptElement>
-            <b>기부자</b>
-            <b>{data.nickname}</b>
-          </ReceiptElement>
-          <ReceiptElement>
-            <b>배송지</b>
-            <b>{data.facilityName}</b>
-          </ReceiptElement>
-          <ReceiptElement>
-            <span>기부일시</span>
-            <span>{data.historyDate}</span>
-          </ReceiptElement>
-          <CustomDashedBorder />
-          <ReceiptElement>
-            <span>배송품목</span>
-            <span>{data.fundingSummary}</span>
-          </ReceiptElement>
-          <CustomDashedBorder />
-          <ReceiptElement>
-            <span>기부금</span>
-            <span>{commaizeNumber(data.donationAmount)}원</span>
-          </ReceiptElement>
-        </div>
-        <Button primary onClick={handleClick}>
-          영수증 공유하기
-        </Button>
-      </main>
-    </>
-  );
+  if (isError) {
+    <FetchError onClick={() => refetch} />;
+  }
+  if (data)
+    return (
+      <>
+        <PageTitleCenter title={''} description={''}></PageTitleCenter>
+        <main className={classes.container}>
+          <div className={classes.receipt} ref={receiptRef}>
+            <h1>이음페이 기부 영수증</h1>
+            <CustomDashedBorder />
+            <h2>{data.fundingTitle}</h2>
+            <CustomDashedBorder />
+            <ReceiptElement>
+              <b>기부자</b>
+              <b>{data.nickname}</b>
+            </ReceiptElement>
+            <ReceiptElement>
+              <b>배송지</b>
+              <b>{data.facilityName}</b>
+            </ReceiptElement>
+            <ReceiptElement>
+              <span>기부일시</span>
+              <span>{data.historyDate}</span>
+            </ReceiptElement>
+            <CustomDashedBorder />
+            <ReceiptElement>
+              <span>배송품목</span>
+              <span>{data.fundingSummary}</span>
+            </ReceiptElement>
+            <CustomDashedBorder />
+            <ReceiptElement>
+              <span>기부금</span>
+              <span>{commaizeNumber(data.donationAmount)}원</span>
+            </ReceiptElement>
+          </div>
+          <Button primary onClick={handleClick}>
+            영수증 공유하기
+          </Button>
+        </main>
+      </>
+    );
 }
