877a71dba16636a945acf05d6ae25f0f77061729
김예지 <fpssj@naver.com>
2024-04-02 16:33:32 +0900

    ✨ feat: 리프레시토큰 로직 변경
    Change-Id: Ibad050731842b6f720396f1841cc1dda2aca38e4

--- fe/web/utils/instance.ts
+++ fe/web/utils/instance.ts
@@ -21,10 +21,11 @@ const axiosAuthApi = (): AxiosInstance => {
         config,
         response: { status },
       } = error;
-      console.log(error.response?.data.data);
+      console.log(error.response.data);
+      console.log(error.response);
       if (
         error.response?.status === 401 &&
-        error.response?.data.data.actionRequired === 'REFRESH_TOKEN'
+        error.response?.data.actionRequired === 'REFRESH_TOKEN'
       ) {
         const originRequest = config;
         axiosAuthApi()
