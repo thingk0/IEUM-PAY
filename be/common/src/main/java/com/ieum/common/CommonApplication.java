f79a406502c62c2a0163893ed8cdf2ebd279aef1
고명성 <audtjd9080@naver.com>
2024-04-02 10:46:54 +0900

    🔧 config: Kafka 활성화 및 기본 설정 추가
    - @EnableKafka 어노테이션 추가하여 Kafka 사용 활성화.
    - Kafka 기본 설정을 위한 KafkaConfig 클래스 추가.
      - Kafka 프로듀서에 대한 기본 설정을 담당하는 kafkaProperties 메서드 구현.
      - KafkaTemplate 빈을 통해 메시지 전송을 위한 설정 완료.

--- be/common/src/main/java/com/ieum/common/CommonApplication.java
+++ be/common/src/main/java/com/ieum/common/CommonApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
+@EnableKafka
 @EnableCaching
 @EnableJpaAuditing
 @EnableFeignClients
