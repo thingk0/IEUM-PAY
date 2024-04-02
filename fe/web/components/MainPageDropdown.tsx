65450a15b70f98cd96ec29af9cba31a9cd257084
이은규 <eung9702@gmail.com>
2024-04-02 12:44:42 +0900

    ✨ feat: 대표카드 등록 기능 구현 완료
    Change-Id: I4ef6f8c58041a2e0e2d75febd73e0ef1d3b7baf0

--- fe/web/components/MainPageDropdown.tsx
+++ fe/web/components/MainPageDropdown.tsx
@@ -6,14 +6,13 @@ import {
   DropdownTrigger,
 } from '@nextui-org/react';
 import { useRouter } from 'next/router';
-// import classes from './MainPageDropdown.module.scss';
 import DropdownMenuIcon from './icons/DropdownMenuIcon';
-import { setMainCard } from '@/api/paymentAxios';
 
-function MainPageDropdown() {
-  // const setMain = (id:number) => {
-  //   setMainCard(id)
-  // }
+interface CardManageProps {
+  focused: number;
+  setMain: (id: number) => void;
+}
+function MainPageDropdown({ focused, setMain }: CardManageProps) {
   const router = useRouter();
   return (
     <Dropdown>
@@ -26,7 +25,9 @@ function MainPageDropdown() {
         <DropdownItem key="new" onClick={() => router.push('/card')}>
           카드 등록
         </DropdownItem>
-        <DropdownItem key="copy">대표 카드 등록</DropdownItem>
+        <DropdownItem key="copy" onClick={() => setMain(focused)}>
+          대표 카드 등록
+        </DropdownItem>
         <DropdownItem key="delete" className="text-danger" color="danger">
           카드 삭제
         </DropdownItem>
