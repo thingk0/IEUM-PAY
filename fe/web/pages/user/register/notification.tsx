26d711418e5609fb5730d2adddf41989c4695403
김예지 <fpssj@naver.com>
2024-04-03 09:35:15 +0900

    ✨ feat: fcm 서비스워커 알림 로직 추가
    Change-Id: Ief3d61f0a6d677bbd325328a414d3f7304727b27

--- fe/web/pages/user/register/notification.tsx
+++ fe/web/pages/user/register/notification.tsx
@@ -2,13 +2,18 @@
 import React, { useEffect } from 'react';
 import { initializeApp } from 'firebase/app';
 import { getMessaging, onMessage, getToken } from 'firebase/messaging';
+import Button from '@/stories/Button';
 
 const Index = () => {
-  const onMessageFCM = async () => {
+  function handleClick() {
     // 브라우저에 알림 권한을 요청합니다.
-    const permission = await Notification.requestPermission();
-    if (permission !== 'granted') return;
-
+    async function getPermission() {
+      const permission = await Notification.requestPermission();
+      if (permission !== 'granted') return;
+    }
+    getPermission();
+  }
+  const onMessageFCM = async () => {
     // 이곳에도 아까 위에서 앱 등록할때 받은 'firebaseConfig' 값을 넣어주세요.
     const firebaseApp = initializeApp({
       apiKey: 'AIzaSyAnHEyqUnFoxudUMWnFvntVMDKZTymOLxw',
@@ -53,6 +58,9 @@ const Index = () => {
   return (
     <div>
       <h1>서비스를 원활하게 이용하기 위해서 알림 권한을 허용해주세요</h1>
+      <Button primary onClick={handleClick}>
+        허용하기
+      </Button>
     </div>
   );
 };
