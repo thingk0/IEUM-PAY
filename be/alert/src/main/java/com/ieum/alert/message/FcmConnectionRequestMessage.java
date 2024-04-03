1404e7269449debc437d1ee9e2c968ee559a7d32
고명성 <audtjd9080@naver.com>
2024-04-03 12:18:22 +0900

    ✨ feat: 송금 알림 메시지, FCM 연결 요청 메시지, 펀딩 완료 메시지 관련 DTO 클래스 추가
    - TransferReceivedMessage, FcmConnectionRequestMessage, FundingCompletedMessage DTO 추가

---
+++ be/alert/src/main/java/com/ieum/alert/message/FcmConnectionRequestMessage.java
@@ -0,0 +1,17 @@
+package com.ieum.alert.message;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class FcmConnectionRequestMessage {
+
+    private String fcmToken;
+    private Long memberId;
+}
