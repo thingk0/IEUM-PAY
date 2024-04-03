bc841158625311bc1ea87bafc0a6d06ce8c77c1b
고명성 <audtjd9080@naver.com>
2024-04-03 12:05:11 +0900

    ♻️ refactor: 카드 등록 및 유효성 검사 요청 데이터 클래스에서 Lombok 어노테이션 제거
    - CardRegisterRequest 클래스 변경: Lombok 어노테이션(@Getter, @RequiredArgsConstructor) 제거하고 record 기본 기능 사용으로 간소화
    - CardValidRequest 클래스 변경: 마찬가지로 Lombok 어노테이션 제거하고 record 기본 기능 사용으로 코드 간소화

--- be/pay/src/main/java/com/ieum/pay/request/CardRegisterRequest.java
+++ be/pay/src/main/java/com/ieum/pay/request/CardRegisterRequest.java
@@ -1,10 +1,5 @@
 package com.ieum.pay.request;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@Getter
-@RequiredArgsConstructor
 public record CardRegisterRequest(String cardNumber, Long memberId, String cardNickname) {
 
 }
--- be/pay/src/main/java/com/ieum/pay/request/CardValidRequest.java
+++ be/pay/src/main/java/com/ieum/pay/request/CardValidRequest.java
@@ -1,10 +1,5 @@
 package com.ieum.pay.request;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@Getter
-@RequiredArgsConstructor
 public record CardValidRequest(Long memberId, Long registeredCardId) {
 
 }
